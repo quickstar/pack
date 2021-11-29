3bdeaf7694bf3916955f3062e89c5a65668f60ed
Lukas Läderach <lukas.laederach@3volutions.ch>
2021-11-29 14:23:58 +0100

    feat(fonts): install Hack Nerd Font if not exists

--- install.sh
+++ install.sh
@@ -9,6 +9,29 @@ if [ -x "$(command -v git)" ] && [ ! -d "${MYHOME}/.tmux/plugins/tpm" ]; then
 	git clone https://github.com/tmux-plugins/tpm "${MYHOME}/.tmux/plugins/tpm"
 fi
 
+font_dir="${MYHOME}/.local/share/fonts/"
+if [ ! -f "${font_dir}Hack Regular Nerd Font Complete.ttf" ]; then
+	mkdir -p $font_dir
+	curl -fLo ${MYHOME}/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+	unzip -d ${font_dir} ${MYHOME}/Hack.zip
+	rm  ${MYHOME}/Hack.zip
+
+	# Reset font cache on Linux
+	if [[ -n $(command -v fc-cache) ]]; then
+	  fc-cache -f "$font_dir"
+	  case $? in
+		[0-1])
+		  # Catch fc-cache returning 1 on a success
+		  exit 0
+		  ;;
+		*)
+		  exit $?
+		  ;;
+	  esac
+	fi
+fi
+
+
 # list of files/folders to symlink in homedir
 declare -a files
 files=(.bashrc)
